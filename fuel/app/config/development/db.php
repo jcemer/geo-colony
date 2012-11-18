31892e9aa47b8e54f14276ddbabca628e5ca9324
Jcemer <jeancarloemer@gmail.com>
2012-11-17 22:44:59 -0200

    local database config

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -6,9 +6,10 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=fuel_dev',
+			'dsn'        => 'mysql:host=localhost;dbname=geo-colony',
 			'username'   => 'root',
-			'password'   => 'root',
+			'password'   => 'z4j9p2',
 		),
+        'profiling'    => true,
 	),
 );
