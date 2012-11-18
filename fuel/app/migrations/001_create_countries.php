9a1387523ac2c8f38e2da8680312b3591fd9e74f
Jcemer <jeancarloemer@gmail.com>
2012-11-17 22:59:55 -0200

    country model

---
+++ fuel/app/migrations/001_create_countries.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Fuel\Migrations;
+
+class Create_countries
+{
+	public function up()
+	{
+		\DBUtil::create_table('countries', array(
+			'id' => array('constraint' => 11, 'type' => 'int', 'auto_increment' => true),
+			'name' => array('constraint' => 255, 'type' => 'varchar'),
+			'code' => array('constraint' => 2, 'type' => 'char'),
+			'created_at' => array('constraint' => 11, 'type' => 'int'),
+			'updated_at' => array('constraint' => 11, 'type' => 'int'),
+
+		), array('id'));
+	}
+
+	public function down()
+	{
+		\DBUtil::drop_table('countries');
+	}
+}
