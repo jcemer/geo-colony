40f8ee730aa4a3ad2c200ae427bc1c17f0954d42
Jean Carlo Emer <jeancarloemer@gmail.com>
2013-02-12 18:58:44 -0200

    restoring to a simple template

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -2,18 +2,15 @@
 <html>
 <head>
 	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<title><?php echo $title; ?></title>
-	<?php echo Asset::css('bootstrap.css'); ?>
-	<style>
-		body { margin: 40px; }
-	</style>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width">
+	<?php echo Asset::css('main.css'); ?>
 </head>
 <body>
-	<div class="container">
-		<div class="row">
-			<div class="span16">
-				<h1><?php echo $title; ?></h1>
-				<hr>
+<?php echo $content; ?>
+	
 <?php if (Session::get_flash('success')): ?>
 				<div class="alert-message success">
 					<p>
@@ -21,6 +18,7 @@
 					</p>
 				</div>
 <?php endif; ?>
+
 <?php if (Session::get_flash('error')): ?>
 				<div class="alert-message error">
 					<p>
@@ -28,18 +26,5 @@
 					</p>
 				</div>
 <?php endif; ?>
-			</div>
-			<div class="span16">
-<?php echo $content; ?>
-			</div>
-		</div>
-		<footer>
-			<p class="pull-right">Page rendered in {exec_time}s using {mem_usage}mb of memory.</p>
-			<p>
-				<a href="http://fuelphp.com">FuelPHP</a> is released under the MIT license.<br>
-				<small>Version: <?php echo e(Fuel::VERSION); ?></small>
-			</p>
-		</footer>
-	</div>
 </body>
 </html>
