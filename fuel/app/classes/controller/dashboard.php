a713f0d700c8220af1df8d3d5bc2248e67a12a9e
Jean Carlo Emer <jeancarloemer@gmail.com>
2013-03-29 21:53:39 -0300

    Fix indentation

--- fuel/app/classes/controller/dashboard.php
+++ fuel/app/classes/controller/dashboard.php
@@ -5,27 +5,27 @@ class Controller_Dashboard extends Controller_Template
 
 	public function action_index()
 	{
-        $colonies_data = Model_Colony::find('all', array(
-            'order_by' => 'name'
-        ));
-        
-        $trails_data = array();
+		$colonies_data = Model_Colony::find('all', array(
+			'order_by' => 'name'
+		));
+		
+		$trails_data = array();
 
-        // adding select option
-        $select_option = array('id' => '-', 'name' => 'Selecione...');
+		// adding select option
+		$select_option = array('id' => '-', 'name' => 'Selecione...');
 
-        array_unshift($colonies_data, $select_option);
-        $colonies_data = Arr::assoc_to_keyval($colonies_data, 'id', 'name');
+		array_unshift($colonies_data, $select_option);
+		$colonies_data = Arr::assoc_to_keyval($colonies_data, 'id', 'name');
 
-        array_unshift($trails_data, $select_option);
-        $trails_data = Arr::assoc_to_keyval($trails_data, 'id', 'name');        
+		array_unshift($trails_data, $select_option);
+		$trails_data = Arr::assoc_to_keyval($trails_data, 'id', 'name');        
 
-        // view
-        $view = View::forge('dashboard/index');
-        $view->set_global('colonies', $colonies_data);
-        $view->set_global('trails', $trails_data);
+		// view
+		$view = View::forge('dashboard/index');
+		$view->set_global('colonies', $colonies_data);
+		$view->set_global('trails', $trails_data);
 
-        // template
+		// template
 		$this->template->title = 'Dashboard &raquo; Index';
 		$this->template->content = $view;
 	}
